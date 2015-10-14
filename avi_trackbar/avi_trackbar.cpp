bbc27bffda8c72524b24436fdebcd08c5679a023
wjxcsu <wjxcsu123@163.com>
2015-10-14 13:12:29 +0800

    添加进度条

--- avi_trackbar/avi_trackbar.cpp
+++ avi_trackbar/avi_trackbar.cpp
@@ -3,10 +3,36 @@
 
 #include "stdafx.h"
 #include<stdio.h>
-
+#include<highgui.h>
+#include<cv.h>
+int g_slider_position = 0;
+CvCapture *g_capture = NULL;
+void onTrackbarSilder(int pos)
+{
+	cvSetCaptureProperty(g_capture,CV_CAP_PROP_POS_FRAMES,pos);
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf("aaaa\n");
-	return 0;
+	cvNamedWindow("avi_trackbar",CV_WINDOW_AUTOSIZE);
+	g_capture = cvCreateFileCapture("D:\\Beyond - π‚ª‘ÀÍ‘¬opencv.avi");
+	int frames = (int)cvGetCaptureProperty(g_capture, CV_CAP_PROP_FRAME_COUNT);
+	if (frames != 0)
+	{
+		cvCreateTrackbar("positons","avi_trackbar",&g_slider_position,frames,onTrackbarSilder);
+	}
+	IplImage *frame;
+	while (1)
+	{
+		frame = cvQueryFrame(g_capture);
+		if (!frame)
+			break;
+		cvShowImage("avi_trackbar",frame);
+		
+		char c = cvWaitKey(33);
+		if (c == 27)
+			break;
+	}
+	cvReleaseCapture(&g_capture);
+	cvDestroyWindow("avi_trackbar");
 }
 
